751e00d448327c4a374031128fb409f9cb59696d
Eszter Szabó <szaboeszter5@gmail.com>
2024-12-06 20:45:27 +0100

    first try

--- 01a/Program.cs
+++ 01a/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace _01a
 {
@@ -10,6 +12,21 @@ namespace _01a
     {
         static void Main(string[] args)
         {
+            string[] all = File.ReadAllLines("input.txt");
+            int[] left = new int[all.Length];
+            int[] right = new int[all.Length];
+            for (int i = 0; i < all.Length; i++)
+            {
+                left[i] = int.Parse(all[i].Split(' ')[0]);
+                right[i] = int.Parse(all[i].Split(' ')[3]);
+            }
+            Array.Sort(left);
+            Array.Sort(right);
+            int sum = 0;
+            for (int i = 0; i < all.Length; i++)
+            {
+                sum += Math.Abs(left[i] - right[i]);
+            }
         }
     }
 }
