62f9344efe89480268b2e0969f99e6c8377f6b7b
Eszter Szabó <szaboeszter5@gmail.com>
2024-12-08 18:57:25 +0100

    i regexed my pants

---
+++ 03a/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _03a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string all = File.ReadAllText("input.txt");
+
+            Regex rg = new Regex(@"(mul)\(\d{1,3},\d{1,3}\)");
+            MatchCollection matchedAuthors = rg.Matches(all);
+
+            List<string[]> numbers = new List<string[]>();
+            foreach (Match match in matchedAuthors)
+            {
+                numbers.Add(match.Value.Replace("mul(","").Replace(")","").Split(','));
+            }
+
+            int sum = 0;
+            foreach (string[] numberArray in numbers)
+            {
+                sum = sum + (int.Parse(numberArray[0]) * int.Parse(numberArray[1]));
+            }
+        }
+    }
+}
