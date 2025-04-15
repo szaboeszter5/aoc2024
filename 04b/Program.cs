3f9b78f2e3800e0f819beb7c860b1f18da311f02
Eszter Szabó <szaboeszter5@gmail.com>
2025-04-15 22:51:05 +0200

    Day 4 Part 2

---
+++ 04b/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04b
+{
+    internal class Program
+    {
+        static int width = 0;
+        static int height = 0;
+
+        static void Main(string[] args)
+        {
+            string[] all = File.ReadAllLines("input.txt");
+            width = all[0].Length;
+            height = all.Length;
+
+            char[,] chars = new char[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    chars[i, j] = all[i][j];
+                }
+            }
+
+            int wordsFound = 0;
+            for (int i = 1; i < height-1; i++)
+            {
+                for (int j = 1; j < width-1; j++)
+                {
+                    if ((chars[i, j] == 'A') && ((chars[i - 1, j - 1] == 'M' && chars[i + 1, j + 1] == 'S') || (chars[i - 1, j - 1] == 'S' && chars[i + 1, j + 1] == 'M')) && ((chars[i - 1, j + 1] == 'M' && chars[i + 1, j - 1] == 'S') || (chars[i - 1, j + 1] == 'S' && chars[i + 1, j - 1] == 'M')))
+                    {
+                        wordsFound++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04b/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("04b")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04b")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("174484d8-be60-4027-905d-14340f708c3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
